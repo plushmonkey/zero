1e25fed388f7df2159cfe9c89c6b25d99e55104f
plushmonkey <plushmonkey.ss@gmail.com>
2025-04-04 11:13:30 -0400

    Add config read nodes
    - Create ReadConfigStringNode and ReadConfigIntNode which will read the
      config with the specific key and output it into the blackboard with
      the output_key.

--- zero/behavior/nodes/BlackboardNode.h
+++ zero/behavior/nodes/BlackboardNode.h
@@ -45,6 +45,47 @@ struct BlackboardEraseNode : public BehaviorNode {
   const char* key;
 };
 
+struct ReadConfigStringNode : public BehaviorNode {
+  ReadConfigStringNode(const char* config_key_name, const char* output_key)
+      : config_key_name(config_key_name), output_key(output_key) {}
+
+  ExecuteResult Execute(ExecuteContext& ctx) override {
+    // Assign standard variables with priority of zone name section then General if that fails.
+    const char* group_lookups[] = {to_string(ctx.bot->server_info.zone), "General"};
+
+    auto opt_key_value = ctx.bot->config->GetString(group_lookups, ZERO_ARRAY_SIZE(group_lookups), config_key_name);
+    if (!opt_key_value) return ExecuteResult::Failure;
+
+    ctx.blackboard.Set<std::string>(output_key, std::string(*opt_key_value));
+
+    return ExecuteResult::Success;
+  }
+
+  const char* config_key_name = nullptr;
+  const char* output_key = nullptr;
+};
+
+template <typename T>
+struct ReadConfigIntNode : public BehaviorNode {
+  ReadConfigIntNode(const char* config_key_name, const char* output_key)
+      : config_key_name(config_key_name), output_key(output_key) {}
+
+  ExecuteResult Execute(ExecuteContext& ctx) override {
+    // Assign standard variables with priority of zone name section then General if that fails.
+    const char* group_lookups[] = {to_string(ctx.bot->server_info.zone), "General"};
+
+    auto opt_key_value = ctx.bot->config->GetInt(group_lookups, ZERO_ARRAY_SIZE(group_lookups), config_key_name);
+    if (!opt_key_value) return ExecuteResult::Failure;
+
+    ctx.blackboard.Set<T>(output_key, (T)*opt_key_value);
+
+    return ExecuteResult::Success;
+  }
+
+  const char* config_key_name = nullptr;
+  const char* output_key = nullptr;
+};
+
 template <typename T>
 struct ValueCompareQuery : public behavior::BehaviorNode {
   ValueCompareQuery(const char* key, T compare_value) : key(key), compare_value(compare_value) {}
