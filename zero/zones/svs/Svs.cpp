353735e0ae990a6e83e8c5ee71cf291a0f001d40
reighnman <jaredo@ameritech.net>
2025-04-04 10:52:49 -0500

    -Added nexus behavior for SVS (for use on Nexus zone pubs)

--- zero/zones/svs/Svs.cpp
+++ zero/zones/svs/Svs.cpp
@@ -5,6 +5,7 @@
 #include <zero/game/Logger.h>
 #include <zero/zones/ZoneController.h>
 #include <zero/zones/svs/PowerballBehavior.h>
+#include <zero/zones/svs/NexusBehavior.h>
 #include <zero/zones/svs/TerrierBehavior.h>
 #include <zero/zones/svs/WarbirdBehavior.h>
 #include <zero/zones/svs/WarzoneBehavior.h>
@@ -34,6 +35,7 @@ void SvsController::CreateBehaviors(const char* arena_name) {
     return;
   }
 
+  repo.Add("nexus", std::make_unique<NexusBehavior>());
   repo.Add("warbird", std::make_unique<WarbirdBehavior>());
   repo.Add("terrier", std::make_unique<TerrierBehavior>());
   repo.Add("warzone", std::make_unique<WarzoneBehavior>());
